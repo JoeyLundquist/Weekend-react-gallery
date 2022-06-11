bd0a589ca5c5a7b8582552b3ae74206a7010e9e1
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-10 21:29:23 -0500

    finished the GalleryItem component

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,13 +1,15 @@
-
+import GalleryItem from "../GalleryItem/GalleryItem";
 
 const GalleryList = ({galleryPhotos}) => {
     return (
         <>
-            Testing my Gallery List
             <ul>
                 {galleryPhotos.map((photo) => {
                    return( 
-                    <li><img src={photo.path} /> </li>
+                        <GalleryItem 
+                            key={photo.id}
+                            photo={photo}
+                        />
                    )
                 })}
             </ul>
